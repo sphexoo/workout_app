508e3fef1db1f247e453d2d3e09f7d5c41594bbd
sphexoo <fabian_sauer@freenet.de>
2024-06-04 20:34:28 +0200

    Added css style for tables

--- project/myworkout/forms.py
+++ project/myworkout/forms.py
@@ -1,4 +1,10 @@
 from django import forms
 
+
 class NumberOfExercisesForm(forms.Form):
-    number_of_exercises = forms.IntegerField(label="Number of Exercises", min_value=1, max_value=10)
+    number_of_exercises = forms.IntegerField(
+        label="Number of Exercises",
+        min_value=1,
+        max_value=10,
+        initial=3,
+    )
